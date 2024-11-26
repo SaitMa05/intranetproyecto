5cb1b6874209eb6960e804abf3d76a835391902d
SaitMa099 <bbvgffcx@gmail.com>
2024-11-26 12:43:06 -0300

    Config

--- core/config.php
+++ core/config.php
@@ -1,11 +1,11 @@
 <?php
 // Configuracion de PhpMail
-define('SMTP_USER', 'asistencias@proyectopropio.uno');
-define('SMTP_PASS', 'hdsG8k@U');
+define('SMTP_USER', '');
+define('SMTP_PASS', '');
 
 
 // Configuración de la base de datos
-define('DB_HOST', 'vps-4445190-x.dattaweb.com');
-define('DB_USER', 'c2650896_asisten');
-define('DB_PASSWORD', '@Sb@ftt7bP');
-define('DB_NAME', 'c2650896_asisten');
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
